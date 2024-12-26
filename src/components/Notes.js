763268ecbd409a2cc6adb1ca2ebdc8d10a42a0e9
Ankana <reach2ankana@gmail.com>
2024-12-26 20:20:20 +0530

    add,delete system

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,16 +1,20 @@
 import { useContext } from 'react'
 import noteContext from "../context/notes/noteContext.js"
 import Noteitem from './Noteitem.js';
+import AddNotes from './AddNotes'
 const Notes = () => {
     const context = useContext(noteContext);
-const {note, setNote} = context;
+const {note, addNote} = context;
   return (
+    <>
+    <AddNotes/>
     <div className='row'>
        <h4>Your Note</h4>
       {note.map((note)=>{
-        return <Noteitem note={note}/>;
+        return <Noteitem key={note._id} note={note}/>;
       })}
     </div>
+    </>
   )
 }
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,8 +1,8 @@
 import NoteContext from "./noteContext";
 import { useState } from "react";
-const NoteState = (props)=>{
 
-const notesinitial = [
+const NoteState = (props) => {
+  const notesinitial = [
     {
       "_id": "675c5e0409e19d75563975f3",
       "user": "675c44ff6b30c464e37b0b73",
@@ -48,15 +48,43 @@ const notesinitial = [
       "date": "2024-12-16T12:11:50.365Z",
       "__v": 0
     }
-  ]
-const [note, setNote] = useState(notesinitial)
+  ];
 
+  const [note, setNote] = useState(notesinitial);
 
-return (
-    <NoteContext.Provider value={{note, setNote }}>
-{props.children}
-</NoteContext.Provider>
-)
-}
+  // Add note
+  const addNote = (title, description, tag) => {
+    console.log("Adding note");
+    const newNote = {
+      "_id": "67601906b965409c9c2ca6111",
+      "user": "675c44ff6b30c464e37b0b73",
+      "title": title,
+      "description": description,
+      "tag": tag,
+      "date": new Date().toISOString(),
+      "__v": 0
+    };
+    setNote([...note, newNote]); // Add the new note to the state
+  };
+
+  // Edit note
+  const editNote = () => {
+    // Edit note logic here
+  };
+
+  // Delete note
+  const deleteNote = (id) => {
+   console.log("deleting nots with id=" +id);
+   const newNotes = note.filter((note) => note._id !== id);
+
+   setNote(newNotes);
+  };
+
+  return (
+    <NoteContext.Provider value={{ note, addNote, editNote, deleteNote }}>
+      {props.children}
+    </NoteContext.Provider>
+  );
+};
 
 export default NoteState;
