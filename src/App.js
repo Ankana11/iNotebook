e1c1ee2fcabac190ee34a8e1957c2902c4452ff5
Ankana <reach2ankana@gmail.com>
2024-12-30 21:18:18 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
-import Alert from './components/Alert';
+// import Alert from './components/Alert';
 
 function App() {
   return (
    <NoteState>
     <Router>
       <Navbar />
-      <Alert/>
+      {/* <Alert/> */}
       <div className="container">
       <Routes>
         <Route exact path="/" element={<Home />} />
